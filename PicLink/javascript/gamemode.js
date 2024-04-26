26eac4b0764637ee20055db83f69b53e49c1228a
jacdiste <distefano.1996080@studenti.uniroma1.it>
2024-04-26 17:48:17 +0200

    a

---
+++ PicLink/javascript/gamemode.js
@@ -0,0 +1,29 @@
+//slideshow temi
+let themeIndex = 1;
+        showThemes(themeIndex);
+            
+        // Next/previous controls
+        function plusThemes(n) {
+          showThemes(themeIndex += n);
+        }
+        
+        // Thumbnail image controls
+        function currentTheme(n) {
+          showThemes(themeIndex = n);
+        }
+        
+        function showThemes(n) {
+          let i;
+          let themes = document.getElementsByClassName("theme");
+          let dots = document.getElementsByClassName("dot");
+          if (n > themes.length) {themeIndex = 1}
+          if (n < 1) {themeIndex = themes.length}
+          for (i = 0; i < themes.length; i++) {
+            themes[i].style.display = "none";
+          }
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          themes[themeIndex-1].style.display = "block";
+          dots[themeIndex-1].className += " active";
+        }
