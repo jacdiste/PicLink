af46b919b14791c95975ad69523fae039b3cdc6f
NicholasMontoni <montoni.2021469@studenti.uniroma1.it>
2024-04-28 19:14:30 +0200

    gamemode

--- PicLink/javascript/gamemode.js
+++ PicLink/javascript/gamemode.js
@@ -15,14 +15,19 @@ function currentTheme(n) {
 function showThemes(n) {
   let i;
   let themes = document.getElementsByClassName("theme");
-  let dots = document.getElementsByClassName("dot");
   if (n > themes.length) {themeIndex = 1}
   if (n < 1) {themeIndex = themes.length}
   for (i = 0; i < themes.length; i++) {
     themes[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   themes[themeIndex-1].style.display = "block";
+}
+
+function toggleDropdown() {
+  const dropdown = document.querySelector(".dropdown");
+  if (dropdown.style.display === "none" || dropdown.style.display === "") {
+      dropdown.style.display = "block";
+  } else {
+      dropdown.style.display = "none";
+  }
 }
