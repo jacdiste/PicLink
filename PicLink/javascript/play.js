2cfdf874f394afd86d45247ca85b307f4d8b12d1
NicholasMontoni <montoni.2021469@studenti.uniroma1.it>
2024-05-21 22:18:57 +0200

    update

--- PicLink/javascript/play.js
+++ PicLink/javascript/play.js
@@ -5,25 +5,111 @@ var tema = parametriGET[0].split("=")[1];
 var level = parametriGET[1].split("=")[1];
 
 var numliv= document.getElementById("numero-livello").innerText="LIVELLO "+level;
-const foto = document.getElementsByClassName("foto");
 
-for (let i=0; i<foto.length; i++) {
-    foto[i].src="../foto/Gioco/Tema "+tema+"/"+level+"/"+(i+1)+".png";
-}
+var images = document.getElementsByClassName("foto");
 
-//Prendo la soluzione e la salvo in una variabile
-var sol;
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var json = JSON.parse(xhr.responseText);
-        sol = json[tema][level-1];
+for (var i=0; i<images.length; i++) {
+    images[i].src="../foto/Gioco/Tema "+tema+"/"+level+"/"+(i+1)+".png";
+    if (i!=0) {
+        images[i].style.visibility = "hidden";
     }
 }
 
-xhr.open("GET", "../foto/Gioco/soluzioni.JSON", true);
-xhr.send();
+const time1 = 10;
+const time2 = 20;
+const time3 = 30;
+
+let timePassed1 = 0;
+let timePassed2 = 0;
+let timePassed3 = 0;
+
+let timeLeft1 = time1;
+let timeLeft2= time2;
+let timeLeft3= time3;
+
+function showTime (time) {
+    var minuti = Math.floor(time / 60);
+
+    var secondi = time % 60;
+    if (secondi < 10) {
+        secondi = "0" + secondi;
+    }
+
+    return minuti + ":" + secondi;
+}
+
+var containers = document.getElementsByClassName("image");
+
+for (let i=1; i<images.length; i++) {
+    containers[i].style.backgroundColor = "grey";
+
+    if (i==1) {
+        containers[i].insertAdjacentHTML("beforeend",`
+            <svg class="timer" id="timer1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="timer-inside">
+                    <circle class="timer-circle" id="timercircle1" cx="50" cy="50" r="45" />
+                </g>
+                <text id="timer-label1" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"> ${showTime(time1)} </text>
+            </svg>`);
+    }
+    else if (i==2) {
+        containers[i].insertAdjacentHTML("beforeend",`
+            <svg class="timer" id="timer2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="timer-inside">
+                    <circle class="timer-circle" id="timercircle2" cx="50" cy="50" r="45" />
+                </g>
+                <text id="timer-label2" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"> ${showTime(time2)} </text>
+            </svg>`);
+    }
+    else {
+        containers[i].insertAdjacentHTML("beforeend",`
+            <svg class="timer" id="timer3" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="timer-inside">
+                    <circle class="timer-circle" id="timercircle3" cx="50" cy="50" r="45" />
+                </g>
+                <text id="timer-label3" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"> ${showTime(time3)} </text>
+            </svg>`);
+    }
+}
+let timer = null;
+startTimer();
+
+function startTimer() {
+    timer = setInterval(() => {
+        if(timeLeft1>0) {
+            timePassed1 += 1;
+            timeLeft1 = time1 - timePassed1;
+            document.getElementById("timer-label1").innerHTML = showTime(timeLeft1);
+        }
+        else {
+            document.getElementById("timer1").style.display = "none";
+            images[1].style.visibility = "visible";
+        }
+
+        if(timeLeft2>0) {
+            timePassed2 += 1;
+            timeLeft2 = time2 - timePassed2;
+            document.getElementById("timer-label2").innerHTML = showTime(timeLeft2);
+        }
+        else {
+            document.getElementById("timer2").style.display = "none";
+            images[2].style.visibility = "visible";
+        }
+
+        if(timeLeft3>0) {
+            timePassed3 += 1;
+            timeLeft3 = time3 - timePassed3;
+            document.getElementById("timer-label3").innerHTML = showTime(timeLeft3);
+        }
+        else {
+            document.getElementById("timer3").style.display = "none";
+            images[3].style.visibility = "visible";
+            return;
+        }
+
+    }, 1000);
+}
+
 
 const back = document.getElementById("back");
 
@@ -38,7 +124,7 @@ function togglePopup() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var json = JSON.parse(xhr.responseText);
-            sol = json[tema][level-1];
+            var sol = json[tema][level-1];
             var input = document.getElementById ("answer-input");
             var answer = input.value.toUpperCase();
             
