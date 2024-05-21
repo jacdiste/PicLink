8adf090b01b0d60c7cf399f353fe2367714fa745
NicholasMontoni <montoni.2021469@studenti.uniroma1.it>
2024-05-21 15:05:49 +0200

    update

--- PicLink/javascript/play.js
+++ PicLink/javascript/play.js
@@ -11,18 +11,55 @@ for (let i=0; i<foto.length; i++) {
     foto[i].src="../foto/Gioco/Tema "+tema+"/"+level+"/"+(i+1)+".png";
 }
 
+//Prendo la soluzione e la salvo in una variabile
+var sol;
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var json = JSON.parse(xhr.responseText);
+        sol = json[tema][level-1];
+    }
+}
+
+xhr.open("GET", "../foto/Gioco/soluzioni.JSON", true);
+xhr.send();
+
 const back = document.getElementById("back");
 
 back.addEventListener("click", () => {
     window.location = "./levels.html?tema="+tema;
 });
 
-function togglePopup() { 
-    const overlay = document.getElementById('popupOverlay'); 
-    if(document.getElementById('answer-input').value == "COLOSSEO") {
-        overlay.classList.toggle('show'); 
+function togglePopup() {
+    var sol;
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(xhr.responseText);
+            sol = json[tema][level-1];
+
+            var answer = document.getElementById('answer-input').value.toUpperCase();
+            const overlay = document.getElementById('popupOverlay');
+
+            if (parseInt(level) == 12) {
+                document.getElementById('prossimolvl').disabled = true;
+            }
+        
+            if(answer==sol) {
+                overlay.classList.toggle('show'); 
+            }
+            else {
+                alert("Si na latrin");
+            }
+        }
     }
-    else {
-        alert("Si na latrin");
-    }
-} 
+
+    xhr.open("GET", "../foto/Gioco/soluzioni.JSON", true);
+    xhr.send();
+}
+
+function next () {
+    window.location = "./play.html?tema="+tema+"&level=" + (parseInt(level)+1);
+}
